e4fea4415429e0dfcfa83bba673e149812936a43
Aaftab <aftabtai123@gmail.com>
2025-04-26 17:33:09 +0530

    final

---
+++ Practice.py
@@ -0,0 +1,12 @@
+Panasonic 1 ton 5 star Inverter
+Annual consumption units : 550 units/year
+
+Price : 40000
+Total Earned:24000
+
+Remaining 40000-24000=16000
+
+Additional Requirements : 
+Stabilizer: 2000
+Ac Installation : 2000
+Total Remaning : 20000
